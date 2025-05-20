9941778ff0a59dc175e37aa4cf770890991e89bc
Colin McNally <99578101+cjmcnally@users.noreply.github.com>
2025-05-19 22:21:13 -0400

    Fixed About Text

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -68,7 +68,7 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            {`I'am a Computer Science student at Worcester Polytechnic Institute
+            {`I'm a Computer Science student at Worcester Polytechnic Institute
             with a passion for building impactful software. I specialize in
             full-stack development and have experience creating web and mobile
             applications. My projects range from AI-powered math visualization
